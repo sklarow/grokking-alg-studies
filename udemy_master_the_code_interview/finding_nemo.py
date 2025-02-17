c148f209cea32c813d98440525e9594cd139e1c1
Allan Sklarow <allan@sklarow.blog.br>
2025-02-17 15:09:02 +0000

    refactor: change name of directory

---
+++ udemy_master_the_code_interview/finding_nemo.py
@@ -0,0 +1,10 @@
+nemo = ['nemo']
+nemo_and_friends = ['doroth', 'john', 'maria', 'nemo']
+
+def finding_nemo (arr):
+    for item in arr:
+        if item == "nemo":
+            print('Nemo was found!')
+
+finding_nemo(nemo)
+finding_nemo(nemo_and_friends)
