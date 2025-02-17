6374d3c2207829599c104a5651859e91dcf1ee52
Allan Sklarow <allan@sklarow.blog.br>
2025-02-17 09:05:52 +0000

    Add recursive countdown example

---
+++ recursive_countdown.py
@@ -0,0 +1,13 @@
+def countdown (i):
+    print (i)  
+    if i <= 1:      # base case
+        return
+    else:           # recursive case
+        countdown(i-1)
+        
+
+countdown(3)
+countdown(-1)
+countdown(0)
+countdown(1)
+countdown(10)
